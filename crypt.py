fea2aa9562c95442a07e66202c8c4cc46fdb2957
Kenneth Chen <74205780+Chenneth@users.noreply.github.com>
2023-04-07 23:27:42 -0500

    Password is hashed with argon2id; now using AES-GCM
    Signed-off-by: Kenneth-W-Chen <74205780+Kenneth-W-Chen@users.noreply.github.com>

--- crypt.py
+++ crypt.py
@@ -1,54 +1,47 @@
-import hashlib
-import os
-import base64
+from argon2 import PasswordHasher
+
+from base64 import b64decode, b64encode
 
 from Crypto.Cipher import AES
-from Crypto import Random
+from Crypto import Random as crand
 from Crypto.Util.Padding import pad, unpad
 
 
 class AESCipher(object):
+    hasher = PasswordHasher()
 
     def __init__(self, key: str):
-        #self.bs = 32
-        self.key = hashlib.sha256(key.encode('utf-8')).digest() #turns the password into a 32char long key
+        # self.bs = 32
+        # self.key = hashlib.sha256(key.encode('utf-8')).digest() #turns the password into a 32char long key
+        self.argon2 = self.hasher.hash(key).split('$')
 
-    def pad(self, s):
-        return pad(s,16)
+        # argon2-cffi encodes the values in base64, so we decode it here to get our byte values
+        self.salt = b64decode(self.argon2[5] + '==')  # Should be 16 bytes long by default
+        self.key = b64decode(self.argon2[5] + '=')  # Should be 32 bytes long
 
-    def remove_pad(self, m):
-        return unpad(m,16)
-        
-        #encrypts plaintext and generates IV (initialization vector)
+    # encrypts plaintext and generates IV (initialization vector)
     def encrypt(self, plaintext):
-        plaintext = self.pad(plaintext)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        return iv + cipher.encrypt(plaintext)
+        cipher = AES.new(self.key, AES.MODE_GCM)
+        return cipher.encrypt_and_digest(plaintext)
 
-        #derypts ciphertexts
-    def decrypt(self, ciphertext):
-        iv = ciphertext[:AES.block_size]
-        cipher = AES.new(self.key, AES.MODE_CBC, iv)
-        plaintext = cipher.decrypt(ciphertext[AES.block_size:])
-        return self.remove_pad(plaintext)
-    
-        #encrypts a file and returns a comment to be posted
+    # decrypts ciphertexts
+    def decrypt(self, ciphertext, mac_tag):
+        cipher = AES.new(self.key, AES.MODE_GCM)
+        return cipher.decrypt_and_verify(ciphertext, mac_tag)
+
+    # encrypts a file and returns a comment to be posted
     def encrypt_file(self, file_path):
-
         with open(file_path, 'rb') as fo:
             plaintext = fo.read()
         enc = self.encrypt(plaintext)
-        comment = base64.b64encode(enc)
-        #comment = enc.decode('ISO-8859-1').encode('ascii')
-        return comment 
-        
-        #takes in a comment to be posted and decrypts it into a file
-    def decrypt_file(self, comment, file_path):
+        # comment = enc.decode('ISO-8859-1').encode('ascii')
+        return [b64encode(enc[0]), b64encode(enc[1])]
 
+        # takes in a comment to be posted and decrypts it into a file
+
+    def decrypt_file(self, comment, file_path):
         ciphertext = base64.b64decode(comment)
-        #ciphertext = comment.decode('ascii').encode('ISO-8859-1')
+        # ciphertext = comment.decode('ascii').encode('ISO-8859-1')
         dec = self.decrypt(ciphertext)
         with open(file_path, 'wb') as fo:
             fo.write(dec)
-
